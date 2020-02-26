a57522c2e143ffe5dab4674952f31bf61c476ed1
Joey Kang <littlekangaroo21@gmail.com>
2020-02-26 10:39:34 -0800

    Ball Intake and Load Subsystems

---
+++ src/main/java/frc/robot/subsystems/BallLoadSubsystem.java
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class BallIntakeSubsystem extends Subsystem {
+  WPI_VictorSPX victorBallLoad; 
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public BallLoadSubsystem() {
+    victorBallLoad = new WPI_VictorSPX(RobotMap.VICTORSPX_ballLoad);
+
+    // TODO: invert motor?
+    // victorBallLoad.setInverted(true);
+  }
+
+  public void intake(double speed) {
+    victorBallLoad.set(speed);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
